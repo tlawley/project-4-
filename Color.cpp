ac98e5d31111a1f661ad8dc0cefbde8713a9a873
tlawley <tlawley@umich.edu>
2017-03-16 23:55:44 -0400

    initial commit

---
+++ Color.cpp
@@ -0,0 +1,34 @@
+/**
+ * Color.cpp
+ *
+ * EECS 183, Winter 2017
+ * Project 4: CoolPics
+ *
+ * <#Name(s)#>
+ * <#uniqname(s)#>
+ *
+ * <#Description#>
+ */
+
+#include "Color.h"
+
+// TODO: implement first checkRange, then two constructors, setRed, getRed,
+//       setGreen, getGreen, setBlue, getBlue, read, write.
+
+
+
+// Your code goes above this line.
+// Don't change the implementations below!
+  
+istream& operator >> (istream& ins, Color& color)
+{
+   color.read(ins);
+   return ins;
+}
+
+ostream& operator << (ostream& outs, Color color)
+{
+   color.write(outs);
+   return outs;
+}
+
---
+++ Point.cpp
@@ -0,0 +1,36 @@
+/**
+ * Point.cpp
+ *
+ * EECS 183, Winter 2017
+ * Project 4: CoolPics
+ *
+ * <#Name(s)#>
+ * <#uniqname(s)#>
+ *
+ * <#Description#>
+ */
+
+#include "Point.h"
+
+// for the declaration of DIMENSION
+#include "utility.h"
+
+// TODO: implement two constructors, setX, getX, setY, getY, read, write, checkRange.
+
+
+
+// Your code goes above this line.
+// Don't change the implementations below!
+
+istream& operator >> (istream& ins,  Point& pt)
+{
+    pt.read(ins);
+    return ins;
+}
+
+ostream& operator<< (ostream& outs, Point pt)
+{
+    pt.write(outs);
+    return outs;
+}
+
